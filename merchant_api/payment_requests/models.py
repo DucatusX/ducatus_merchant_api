a2392b8366c6b30fc2613f72f23196dcb590ea71
Egor14 <egorgrekhnev@mail.ru>
2020-03-13 19:18:48 +0300

    unique pair shop and cart_id

--- merchant_api/payment_requests/models.py
+++ merchant_api/payment_requests/models.py
@@ -25,3 +25,6 @@ class PaymentRequest(models.Model):
     receive_amount = models.DecimalField(max_digits=MAX_DIGITS, decimal_places=0, default=Decimal('0'))
     state = models.CharField(max_length=50, null=True, default='WAITING_FOR_PAYMENT')
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('shop', 'cart_id',)
