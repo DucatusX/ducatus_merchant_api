b37fdf7104749af163f80064fc52232ba477a7b7
Egor14 <egorgrekhnev@mail.ru>
2020-03-20 18:08:58 +0300

    amount transfer decimals fix

--- merchant_api/litecoin_rpc.py
+++ merchant_api/litecoin_rpc.py
@@ -5,7 +5,6 @@ from merchant_api.consts import DECIMALS
 
 
 class DucatuscoreInterface:
-
     endpoint = None
     settings = None
 
@@ -52,7 +51,7 @@ class DucatuscoreInterface:
 
             transaction_fee = self.rpc.getinfo()['paytxfee']
 
-            output_params = {address: amount - transaction_fee}
+            output_params = {address: amount / DECIMALS['DUC'] - transaction_fee}
 
             tx = self.rpc.createrawtransaction(input_params, output_params)
             print('raw tx', tx, flush=True)
