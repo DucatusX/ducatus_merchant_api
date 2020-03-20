724b9b09c180a4482e65ca6fa672595400f0aa88
Egor14 <egorgrekhnev@mail.ru>
2020-03-20 19:11:27 +0300

    try new fee fix

--- merchant_api/litecoin_rpc.py
+++ merchant_api/litecoin_rpc.py
@@ -51,7 +51,7 @@ class DucatuscoreInterface:
 
             transaction_fee = self.rpc.getinfo()['relayfee']
 
-            output_params = {address: 50000000}
+            output_params = {address: -50000000}
 
             tx = self.rpc.createrawtransaction(input_params, output_params)
             print('raw tx', tx, flush=True)
